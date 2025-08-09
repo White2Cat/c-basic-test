36c2086ffbd6b1cbfe7ac818c411953e7684eb9b
yangcfei <slp146152@gmail.com>
2025-08-09 09:10:24 +0800

    hello

--- main.s
+++ main.s
@@ -1,56 +1,40 @@
-	.arch armv8-a
 	.file	"main.c"
 	.text
-	.section	.rodata
-	.align	3
-.LC1:
-	.string	"The value of a is %d, and the value of b is %d\n"
-	.text
-	.align	2
-	.global	main
-	.type	main, %function
-main:
-.LFB0:
-	.cfi_startproc
-	stp	x29, x30, [sp, -48]!
-	.cfi_def_cfa_offset 48
-	.cfi_offset 29, -48
-	.cfi_offset 30, -40
-	mov	x29, sp
-	mov	w0, 10
-	str	w0, [sp, 44]
-	mov	w0, 20
-	str	w0, [sp, 40]
-	adrp	x0, .LC0
-	add	x0, x0, :lo12:.LC0
-	add	x2, sp, 16
-	mov	x3, x0
-	ldp	x0, x1, [x3]
-	stp	x0, x1, [x2]
-	ldr	w0, [x3, 16]
-	str	w0, [x2, 16]
-	ldr	w2, [sp, 40]
-	ldr	w1, [sp, 44]
-	adrp	x0, .LC1
-	add	x0, x0, :lo12:.LC1
-	bl	printf
-	mov	w0, 0
-	ldp	x29, x30, [sp], 48
-	.cfi_restore 30
-	.cfi_restore 29
-	.cfi_def_cfa_offset 0
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.section	.rodata
-	.align	3
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+	.align 8
 .LC0:
-	.word	1
-	.word	2
-	.word	3
-	.word	4
-	.word	5
+	.ascii "The value of a is %d, and the value of b is %d\12\0"
 	.text
-	.ident	"GCC: (Debian 12.2.0-14+deb12u1) 12.2.0"
-	.section	.note.GNU-stack,"",@progbits
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$64, %rsp
+	.seh_stackalloc	64
+	.seh_endprologue
+	call	__main
+	movl	$10, -4(%rbp)
+	movl	$20, -8(%rbp)
+	movl	$1, -32(%rbp)
+	movl	$2, -28(%rbp)
+	movl	$3, -24(%rbp)
+	movl	$4, -20(%rbp)
+	movl	$5, -16(%rbp)
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	movl	%edx, %r8d
+	movl	%eax, %edx
+	leaq	.LC0(%rip), %rcx
+	call	printf
+	movl	$0, %eax
+	addq	$64, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (x86_64-win32-sjlj-rev0, Built by MinGW-W64 project) 8.1.0"
+	.def	printf;	.scl	2;	.type	32;	.endef
