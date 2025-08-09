eab1feb4a1d415ab2a3dbda18ac332506f55e90c
feiy <slp146152@gmail.com>
2025-08-08 15:50:12 -0900

    c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int a = 10;
+	int b = 20;
+	int arr[] = {1, 2, 3, 4, 5};
+	printf("The value of a is %d, and the value of b is %d\n", a, b);
+
+	return 0;
+}
---
+++ main.s
@@ -0,0 +1,56 @@
+	.arch armv8-a
+	.file	"main.c"
+	.text
+	.section	.rodata
+	.align	3
+.LC1:
+	.string	"The value of a is %d, and the value of b is %d\n"
+	.text
+	.align	2
+	.global	main
+	.type	main, %function
+main:
+.LFB0:
+	.cfi_startproc
+	stp	x29, x30, [sp, -48]!
+	.cfi_def_cfa_offset 48
+	.cfi_offset 29, -48
+	.cfi_offset 30, -40
+	mov	x29, sp
+	mov	w0, 10
+	str	w0, [sp, 44]
+	mov	w0, 20
+	str	w0, [sp, 40]
+	adrp	x0, .LC0
+	add	x0, x0, :lo12:.LC0
+	add	x2, sp, 16
+	mov	x3, x0
+	ldp	x0, x1, [x3]
+	stp	x0, x1, [x2]
+	ldr	w0, [x3, 16]
+	str	w0, [x2, 16]
+	ldr	w2, [sp, 40]
+	ldr	w1, [sp, 44]
+	adrp	x0, .LC1
+	add	x0, x0, :lo12:.LC1
+	bl	printf
+	mov	w0, 0
+	ldp	x29, x30, [sp], 48
+	.cfi_restore 30
+	.cfi_restore 29
+	.cfi_def_cfa_offset 0
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.section	.rodata
+	.align	3
+.LC0:
+	.word	1
+	.word	2
+	.word	3
+	.word	4
+	.word	5
+	.text
+	.ident	"GCC: (Debian 12.2.0-14+deb12u1) 12.2.0"
+	.section	.note.GNU-stack,"",@progbits
